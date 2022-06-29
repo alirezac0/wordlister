33ae51bcbc46adf260e357e7fd209ac6cd50845f
Alireza <32331449+alirezac0@users.noreply.github.com>
2022-06-29 16:23:18 +0430

    Update wordlister.py

--- wordlister.py
+++ wordlister.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 
 paths = []
 words = []
-
+queries = []
 
 if args.target:
     if (os.path.exists(args.target)):
@@ -34,31 +34,38 @@ if args.target:
                 if url[0:7] == "http://" or url[0:8] == "https://":
                     o = urlparse(url)
                     paths.append(o.path)
+                    queries.append(o.query)
             list_set = set(paths)
             unique_paths = (list(list_set))
             for url in unique_paths:
                 url = url.split("/")
-                for peyman in url:
-                    if len(peyman) > 0:
-                        words.append(peyman)
+                for chare in url:
+                    if len(chare) > 0:
+                        words.append(chare)
                         s = ""
-                        if len(peyman) > 0:
-                            for i in range(len(peyman)):
-                                if peyman[i].islower():
-                                    s += peyman[i]
-                                elif  peyman[i].isupper():
+                        if len(chare) > 0:
+                            for i in range(len(chare)):
+                                if chare[i].islower():
+                                    s += chare[i]
+                                elif  chare[i].isupper():
                                     if len(s) > 1:
                                         words.append(s)
-                                    s = peyman[i]
-                                elif peyman[i] == '.' and len(s) > 0:
+                                    s = chare[i]
+                                elif chare[i] == '.' and len(s) > 0:
                                     words.append(s)
-                                if len(s) > 0 and i == len(peyman)-1 and re.search("[A-Z]", s) and not bool(re.search("[\.]", s)):
+                                if len(s) > 0 and i == len(chare)-1 and re.search("[A-Z]", s) and not bool(re.search("[\.]", s)):
                                     words.append(s)
+            list_set3 = set(queries)
+            unique_queries = (list(list_set3))
+            for query in unique_queries:
+                a = query.split("&")
+                for name in a:
+                    words.append(name.split("=")[0])
             list_set2 = set(words)
             unique_words = (list(list_set2))
     else:
         print("Wrong Input!")
 
 for x in unique_words:
-    if len(x) > 1 and not bool(re.search("[\.]", x)):
+    if len(x) > 0 and not bool(re.search("[\.]", x)):
         print(x)
